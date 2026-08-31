e75027de024b707b420d202ab32af1652621bed1
Motheo-mto <motsemmemotheo@gmail.com>
2026-08-31 20:37:57 +0200

    Fixed function return types

--- EventUnit.h
+++ EventUnit.h
@@ -10,7 +10,7 @@ public:
     EventUnit();
     EventUnit(string name, string status, int capacity);
     virtual void update(string status, int capacity) = 0;
-    virtual string reportStatus() const = 0;
+    virtual void reportStatus() const = 0;
     virtual int getCapacity() const = 0;
 };
 
